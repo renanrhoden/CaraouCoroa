a3423894897439ea59ee5109ceaf278a380dad40
Renan Rhoden <renanrhoden@gmail.com>
2017-07-19 00:44:40 -0300

    set facebook ads and minor improvements to design

--- app/src/main/java/com/renanrhoden/caraoucoroa/DetalheActivity.java
+++ app/src/main/java/com/renanrhoden/caraoucoroa/DetalheActivity.java
@@ -3,11 +3,18 @@ package com.renanrhoden.caraoucoroa;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.LinearLayout;
+
+import com.facebook.ads.*;
+
 
 import java.util.Random;
 
@@ -16,12 +23,18 @@ public class DetalheActivity extends AppCompatActivity {
     private ImageView imagem;
     private ImageView botaoVoltar;
     private String[] opcao = {"Cara", "Coroa"};
+    private AdView adView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalhe);
+        adView = new AdView(this, getResources().getString(R.string.placement_id_banner), AdSize.BANNER_HEIGHT_50);
+        LinearLayout adContainer = (LinearLayout) findViewById(R.id.banner_container);
+        adContainer.addView(adView);
+        AdSettings.addTestDevice("f2e5da66c3d39d36f1a9eb74b8c4a5e6");
+        adView.loadAd();
 
         imagem = (ImageView) findViewById(R.id.imagemID);
         botaoVoltar = (ImageView) findViewById(R.id.botaoVoltarID);
@@ -42,15 +55,27 @@ public class DetalheActivity extends AppCompatActivity {
     }
     private void setImage(String s) {
         if ( s.equals("Cara") ){
-            showToast(s);
+            imagem.setVisibility(View.INVISIBLE);
+            Animation anim = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
+            imagem.setAnimation(anim);
+            anim.start();
+            imagem.setVisibility(View.VISIBLE);
             imagem.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.moeda_cara));
         }else{
-            showToast(s);
+            imagem.setVisibility(View.INVISIBLE);
+            Animation anim = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
+            imagem.setAnimation(anim);
+            anim.start();
+            imagem.setVisibility(View.VISIBLE);
             imagem.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.moeda_coroa));
         }
     }
 
-    private void showToast(String s) {
-        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    @Override
+    protected void onDestroy() {
+        if (adView != null) {
+            adView.destroy();
+        }
+        super.onDestroy();
     }
 }
